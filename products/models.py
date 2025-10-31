d7e02f3993a33f6c75e2745019f9c938cf5217a3
kickcik <4778009@gmail.com>
2025-10-31 16:15:22 +0900

    black formatting

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from utils.models import TimestampModel
 
 
@@ -6,46 +7,50 @@ class Tag(models.Model):
     tag_name = models.CharField(max_length=15, null=False, blank=False, unique=True)
 
     class Meta:
-        db_table = 'tags'
-        verbose_name = '태그'
-        verbose_name_plural = '태그 목록'
+        db_table = "tags"
+        verbose_name = "태그"
+        verbose_name_plural = "태그 목록"
 
     def __str__(self):
         return self.tag_name
+
 
 class Category(models.Model):
     category_name = models.CharField(max_length=15, null=False, blank=False, unique=True)
 
     class Meta:
-        db_table = 'categories'
-        verbose_name = '카테고리'
-        verbose_name_plural = '카테고리 목록'
+        db_table = "categories"
+        verbose_name = "카테고리"
+        verbose_name_plural = "카테고리 목록"
 
     def __str__(self):
         return self.category_name
+
 
 class Brand(models.Model):
     brand_name = models.CharField(max_length=100, null=False, blank=False, unique=True)
 
     class Meta:
-        db_table = 'brands'
-        verbose_name = '브랜드'
-        verbose_name_plural = '브랜드 목록'
+        db_table = "brands"
+        verbose_name = "브랜드"
+        verbose_name_plural = "브랜드 목록"
 
     def __str__(self):
         return self.brand_name
+
 
 class BrandImage(TimestampModel):
     brand_image = models.ImageField(null=False, blank=False, unique=True)
     brand_id = models.ForeignKey(Brand, on_delete=models.SET_NULL)
 
     class Meta:
-        db_table = 'brand_images'
-        verbose_name = '브랜드 이미지'
-        verbose_name_plural = '브랜드 이미지 목록'
+        db_table = "brand_images"
+        verbose_name = "브랜드 이미지"
+        verbose_name_plural = "브랜드 이미지 목록"
 
     def __str__(self):
-        return f'{self.brand_id.brand_name} - {self.brand_id}'
+        return f"{self.brand_id.brand_name} - {self.brand_id}"
+
 
 class Product(TimestampModel):
     product_name = models.CharField(max_length=100, null=False, blank=False)
@@ -59,38 +64,40 @@ class Product(TimestampModel):
     brand_id = models.ForeignKey(Brand, on_delete=models.SET_NULL)
 
     class Meta:
-        ordering = ['-created_at']
-        db_table = 'products'
-        verbose_name = '상품'
-        verbose_name_plural = '상품목록'
+        ordering = ["-created_at"]
+        db_table = "products"
+        verbose_name = "상품"
+        verbose_name_plural = "상품목록"
 
     def __str__(self):
         return self.product_name
+
 
 class ProductImage(TimestampModel):
     product_image = models.ImageField(null=False, blank=False, unique=True)
     product_id = models.ForeignKey(Product, on_delete=models.SET_NULL)
 
     class Meta:
-        db_table = 'product_images'
-        verbose_name = '상품 이미지'
-        verbose_name_plural = '상품 이미지 목록'
+        db_table = "product_images"
+        verbose_name = "상품 이미지"
+        verbose_name_plural = "상품 이미지 목록"
 
     def __str__(self):
-        return f'{self.product_id.product_name} - {self.product_id}'
+        return f"{self.product_id.product_name} - {self.product_id}"
+
 
 class ProductQna(TimestampModel):
     question_type = models.CharField(max_length=15, null=False, blank=False)
     question_title = models.CharField(max_length=50, null=False, blank=False)
     question_content = models.TextField(null=False, blank=False)
     question_answer = models.TextField(null=True, blank=True)
-    user_id = models.ForeignKey('User', on_delete=models.SET_NULL)
+    user_id = models.ForeignKey("User", on_delete=models.SET_NULL)
     product_id = models.ForeignKey(Product, on_delete=models.SET_NULL)
 
     class Meta:
-        db_table = 'product_qna'
-        verbose_name = '상품 문의'
-        verbose_name_plural = '상품 문의 목록'
+        db_table = "product_qna"
+        verbose_name = "상품 문의"
+        verbose_name_plural = "상품 문의 목록"
 
     def __str__(self):
-        return f'[{self.question_type}] {self.question_title}'
+        return f"[{self.question_type}] {self.question_title}"
