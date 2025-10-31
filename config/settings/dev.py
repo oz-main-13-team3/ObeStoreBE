48de34489521c1cb6a4ffc4fc82553302bb5d864
kickcik <4778009@gmail.com>
2025-10-31 16:08:28 +0900

    데이터베이스 이름 변경

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -8,7 +8,7 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "viral_db"),
+        "NAME": os.getenv("DB_NAME", "obestore_db"),
         "USER": os.getenv("DB_USER", "postgres"),
         "PASSWORD": os.getenv("DB_PASSWORD", "password1234"),
         "HOST": os.getenv("DB_HOST", "127.0.0.1"),  # docker-compose의 서비스명
