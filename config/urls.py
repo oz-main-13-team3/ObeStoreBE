6026cfbc2e5cf9c44b39a394df143384d9b2785d
daebagi <daeburm0115@naver.com>
2025-11-04 14:43:55 +0900

    장바구니 테스트용 url 작성

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/', include('carts.urls')), # 추후 이름 수정 예정
 ]
