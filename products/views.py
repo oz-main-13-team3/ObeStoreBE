674190a0da37b8769f4fffbb8762cb54b64d93d5
kickcik <4778009@gmail.com>
2025-11-04 10:41:25 +0900

    상품 검색 / 필터링 작업 완료

--- products/views.py
+++ products/views.py
@@ -1,2 +1,25 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
-# Create your views here.
+from products.filters import ProductFilter
+from products.models import Product
+from products.serializer import ProductSerializer
+
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = ProductFilter
+    search_fields = ["product_name"]
+    ordering_fields = ["sales", "product_value", "created_at", "review_count"]
+    ordering = ["-created_at"]  # 기본 정렬
+
+    def get_queryset(self):
+        queryset = Product.objects.select_related("category", "tag", "brand").all()
+        ordering_param = self.request.query_params.get("ordering", "")
+
+        if "review_count" in ordering_param:
+            from django.db.models import Count
+
+            queryset = queryset.annotate(review_count=Count("review"))
+        return queryset
