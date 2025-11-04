c969dbad409193ef32965c8c5de143f26eaa42ed
kickcik <4778009@gmail.com>
2025-11-04 16:40:48 +0900

    load_dotenv() 복원

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,18 +1,21 @@
 import os
 
-from config.settings.base import *
+from dotenv import load_dotenv
+
+from config.settings.base import *  # noqa
 
 DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 
+load_dotenv()
 
 DATABASES = {
-	"default": {
-		"ENGINE": "django.db.backends.postgresql",
-		"NAME": os.getenv("DB_NAME", "obestore_db"),
-		"USER": os.getenv("DB_USER", "postgres"),
-		"PASSWORD": os.getenv("DB_PASSWORD", "password1234"),
-		"HOST": os.getenv("DB_HOST", "127.0.0.1"),
-		"PORT": os.getenv("DB_PORT", "5432"),
-		}
-	}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "obestore_db"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "password1234"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+    }
+}
