c26526c0c80f152fa3329893a51e6bc143f0b6ec
grrr127 <qrrr127@gmail.com>
2025-11-06 10:37:45 +0900

    충돌 병합 해결
    # Conflicts:
    #	users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-from .models import User
+from .models import User, Address, Point
 
 
 class MyUserCreationForm(UserCreationForm):
@@ -14,38 +14,32 @@ class MyUserCreationForm(UserCreationForm):
 class MyUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
-        fields = (
-            "email",
-            "username",
-            "nickname",
-            "phone_number",
-            "status",
-            "is_staff",
-            "is_superuser",
-            "groups",
-            "user_permissions",
-        )
+        fields = ("email", "username", "nickname", "phone_number", "status", "is_staff", "is_superuser", "groups", "user_permissions")
 
+class AddressInline(admin.TabularInline):
+    model = Address
+    extra = 0
+    fields = ("address_name", "recipient", "recipient_phone", "post_code", "address", "detail_address", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+    show_change_link = True
+    ordering = ("updated_at",)
+
+class PointInline(admin.TabularInline):
+    model = Point
+    extra = 0
+    fields = ("balance", "amount", "created_at", "updated_at")
+    readonly_fields = ("balance", "amount", "created_at", "updated_at")
+    can_delete = False
+    ordering = ("updated_at", "-id")
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     add_form = MyUserCreationForm
     form = MyUserChangeForm
     model = User
+    inlines = [AddressInline, PointInline]
 
-    list_display = (
-        "id",
-        "email",
-        "username",
-        "nickname",
-        "phone_number",
-        "status",
-        "is_active",
-        "is_staff",
-        "is_superuser",
-        "created_at",
-        "updated_at",
-    )
+    list_display = ("id", "email", "username", "nickname", "phone_number", "status", "is_active", "is_staff", "is_superuser", "created_at", "updated_at")
     list_filter = ("status", "is_staff", "is_superuser", "groups")
     search_fields = ("email", "username", "nickname", "phone_number")
     ordering = ("id",)
@@ -53,36 +47,36 @@ class UserAdmin(BaseUserAdmin):
     readonly_fields = ("created_at", "updated_at")
 
     fieldsets = (
-        ("기본 정보", {"fields": ("email", "username", "nickname", "phone_number", "password")}),
-        ("권한", {"fields": ("status", "is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
-        ("기타", {"fields": ("last_login", "created_at", "updated_at")}),
+        ("기본 정보", {
+            "fields": ("email", "username", "nickname", "phone_number", "password")
+        }),
+        ("권한", {
+            "fields": ("status", "is_staff", "is_superuser", "groups", "user_permissions")
+        }),
+        ("기타", {
+            "fields": ("last_login", "created_at", "updated_at")
+        }),
     )
 
     add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "email",
-                    "username",
-                    "nickname",
-                    "phone_number",
-                    "password1",
-                    "password2",
-                    "status",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                ),
-            },
-        ),
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email",
+                "username",
+                "nickname",
+                "phone_number",
+                "password1",
+                "password2",
+                "status",
+                "is_staff",
+                "is_superuser",
+                "groups",
+            ),
+        }),
     )
-
-    # 다중 선택 UI (그룹/권한)
     filter_horizontal = ("groups", "user_permissions")
 
-    # [액션] 리스트에서 체크된 유저들을 일괄 상태 변경
     @admin.action(description="선택한 사용자를 활성화(active)로 변경")
     def mark_active(self, request, queryset):
         updated = queryset.update(status="active")
@@ -98,8 +92,32 @@ class UserAdmin(BaseUserAdmin):
         updated = queryset.update(status="dormancy")
         self.message_user(request, f"{updated}명의 사용자가 휴면 처리되었습니다.")
 
-    actions = (
-        "mark_active",
-        "mark_ready",
-        "mark_dormancy",
-    )
+    actions = ("mark_active", "mark_ready", "mark_dormancy",)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "address_name", "recipient", "recipient_phone", "post_code", "address", "detail_address", "created_at", "updated_at")
+    search_fields = ("user__email",)
+    autocomplete_fields = ("user",)
+    list_select_related = ("user",)
+    readonly_fields = ()
+
+@admin.register(Point)
+class PointAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "balance", "amount", "created_at", "updated_at")
+    search_fields = ("user__email",)
+    list_select_related = ("user",)
+    autocomplete_fields = ("user",)
+
+    readonly_fields = ("user", "balance", "amount", "created_at", "updated_at")
+
+    # 수동으로 포인트 추가 금지
+    def has_add_permission(self, request):
+        return False
+    # 포인트 수정 금지
+    def has_change_permission(self, request, obj=None):
+        return False
+    # 포인트 삭제 금지
+    def has_delete_permission(self, request, obj=None):
+        return False
