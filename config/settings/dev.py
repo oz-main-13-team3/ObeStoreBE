2b30c2bf4f5c1740bc2d1e71eca491f8818a0246
kickcik <4778009@gmail.com>
2025-11-06 18:21:17 +0900

    aws 설정

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -19,3 +19,16 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
+
+# AWS Credentials
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = "ap-northeast-2"
+
+AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+# 파일 저장소 기본값을 S3로 변경
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
