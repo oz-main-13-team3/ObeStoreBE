4026a11420346aba535531ab72ef741633ba5bf3
kickcik <4778009@gmail.com>
2025-11-07 16:51:01 +0900

    시크릿 키 참조

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rl4%cu7!zy!v1skem29i!cx=_kz+1c1^dok!g)k=-d2i9k#7if"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
