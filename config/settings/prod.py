2b4ef764f487dd5b4d1eb2da7a65ef07a742b3c2
kickcik <4778009@gmail.com>
2025-11-07 16:53:08 +0900

    prod.py에 설정 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,9 +1,13 @@
 import os
 
+from dotenv import load_dotenv
+
 from config.settings.base import *  # noqa
 
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
+
+load_dotenv()
 
 DATABASES = {
     "default": {
@@ -15,3 +19,31 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
+
+# DJANGO STORAGES
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {
+            "access_key": os.getenv("AWS_ACCESS_KEY_ID"),
+            "secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
+            "bucket_name": os.getenv("AWS_STORAGE_BUCKET_NAME"),
+            "region_name": "ap-northeast-2",
+            "default_acl": "public-read",
+            "querystring_auth": False,
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+# AWS Credentials
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = "ap-northeast-2"
+
+AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
+AWS_S3_SIGNATURE_VERSION = "s3v4"
