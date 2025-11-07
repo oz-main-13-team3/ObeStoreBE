c553d58251d9a5cee2aa44defc30c2093820b014
kickcik <4778009@gmail.com>
2025-11-07 17:33:51 +0900

    prod.py 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -12,10 +12,10 @@ load_dotenv()
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "viral_db"),
+        "NAME": os.getenv("DB_NAME", "obestore_db"),
         "USER": os.getenv("DB_USER", "postgres"),
         "PASSWORD": os.getenv("DB_PASSWORD", "password1234"),
-        "HOST": os.getenv("DB_HOST", "localhost"),  # docker-compose의 서비스명
+        "HOST": os.getenv("DB_HOST", "db"),  # docker-compose의 서비스명
         "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
