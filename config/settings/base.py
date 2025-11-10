8da5a91057a6ebdb0a3ff8f78e9259fe83a23ab6
daebagi <daeburm0115@naver.com>
2025-11-10 14:07:28 +0900

    이메일 인증용 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -173,9 +173,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "3.obestore@gmail.com"
-EMAIL_HOST_PASSWORD = "vslx sctc zyfs swem"
-FRONTEND_BASE_URL = "http://127.0.0.1:8000"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+FRONTEND_BASE_URL = os.getenv("FRONTEND_BASE_URL")
 
 # 네이버 소셜로그인용
 load_dotenv()
