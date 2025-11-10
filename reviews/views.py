683ddb3f60b200f8215a5daecbe12b42feaf6edb
kickcik <4778009@gmail.com>
2025-11-10 15:14:38 +0900

    필터, 정렬 추가

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,9 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
+from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
 
+from reviews.filters import ReviewFilter
 from reviews.models import Keyword, Review, ReviewImage, ReviewKeyword
 from reviews.serializers import (
     KeywordSerializer,
@@ -13,6 +16,16 @@ from reviews.serializers import (
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = ReviewFilter
+
+    ordering_fields = ["rating", "created_at", "product_review_count"]
+    ordering = ["-created_at"]  # 기본 정렬: 최신 등록순
+
+    def get_queryset(self):
+        from django.db.models import Count
+
+        return Review.objects.annotate(product_review_count=Count("product__product_reviews"))
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
