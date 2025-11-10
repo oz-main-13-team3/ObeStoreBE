8fcc030001b4467bdea1daafd33cb49cb9ab3fcc
kickcik <4778009@gmail.com>
2025-11-10 18:42:25 +0900

    poetry caches 정리 추가

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,11 @@ set -e
 
 export DJANGO_SETTINGS_MODULE=config.settings.prod
 
+echo "=== Clearing All Poetry Caches ==="
+RUN /root/.local/bin/poetry install --no-interaction --no-ansi --no-root \
+    && rm -rf /root/.cache/pypoetry/* \
+    && rm -rf /root/.cache/pip/*
+
 echo "=== Running Django migrations ==="
 /root/.local/bin/poetry run python manage.py makemigrations --noinput || true
 /root/.local/bin/poetry run python manage.py migrate --noinput
