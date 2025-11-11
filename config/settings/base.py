8d6d39b4d871592fc18f6f7e10c94ef85d20362a
daebagi <daeburm0115@naver.com>
2025-11-11 15:05:30 +0900

    redirect_uri www 붙이기

--- config/settings/base.py
+++ config/settings/base.py
@@ -191,7 +191,7 @@ load_dotenv()
 
 NAVER_CLIENT_ID = os.getenv("NAVER_CLIENT_ID")
 NAVER_CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET")
-NAVER_REDIRECT_URI = "https://obstore.o-r.kr:8000/users/oauth/naver/callback/"
+NAVER_REDIRECT_URI = "https://wwww.obstore.o-r.kr:8000/users/oauth/naver/callback/"
 
 # 포인트 적립
 REVIEW_REWARD_RATE = Decimal("0.10")
