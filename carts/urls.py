d5ac77161e998a3edc5be17a19aebf044e0e975d
daebagi <daeburm0115@naver.com>
2025-11-13 10:21:32 +0900

    cart url api명세서에 맞추기

--- carts/urls.py
+++ carts/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import CartItemViewSet, CartViewSet
 
 router = DefaultRouter()
-router.register(r"carts", CartViewSet, basename="cart")
-router.register(r"cart-items", CartItemViewSet, basename="cart-item")
+router.register(r"", CartViewSet, basename="cart")
+router.register(r"items", CartItemViewSet, basename="cart-item")
 
 urlpatterns = router.urls
