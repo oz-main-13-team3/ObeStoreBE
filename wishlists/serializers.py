c55304d9748ece6f85c9d64e7d0106ad7634df35
kickcik <4778009@gmail.com>
2025-11-20 03:07:08 +0900

    상품 리스트 Nested 설계

--- wishlists/serializers.py
+++ wishlists/serializers.py
@@ -1,14 +1,14 @@
 # wishlist/serializers.py
 from rest_framework import serializers
 
+from products.serializers import ProductListSerializer
+
 from .models import Wishlist
 
 
 class WishlistSerializer(serializers.ModelSerializer):
-    product_name = serializers.CharField(source="product.product_name", read_only=True)
-    product_value = serializers.DecimalField(source="product.price", max_digits=10, decimal_places=2, read_only=True)
-    brand_id = serializers.IntegerField(source="product.brand.id", read_only=True)
+    product = ProductListSerializer(read_only=True)
 
     class Meta:
         model = Wishlist
-        fields = ["id", "product", "product_name", "product_value", "brand_id"]
+        fields = ["id", "product"]
