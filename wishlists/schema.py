076c67412616b9009d3415e7d574c057273fc7eb
kickcik <4778009@gmail.com>
2025-11-20 03:08:36 +0900

    swagger ui 문서화

--- wishlists/schema.py
+++ wishlists/schema.py
@@ -4,12 +4,12 @@ from wishlists.serializers import WishlistSerializer
 
 wishlists_schema = {
     "create": extend_schema(
-        summary="상품 리뷰 생성", description="상품 리뷰를 생성합니다.", responses=OpenApiResponse(WishlistSerializer)
+        summary="찜 목록 추가", description="상품을 찜 목록에 추가합니다.", responses=OpenApiResponse(WishlistSerializer)
     ),
     "list": extend_schema(
-        summary="상품 리뷰 조회", description="상품 리뷰를 조회합니다.", responses=OpenApiResponse(WishlistSerializer)
+        summary="찜 목록 조회", description="찜 목록을 조회합니다.", responses=OpenApiResponse(WishlistSerializer)
     ),
     "destroy": extend_schema(
-        summary="상품 리뷰 삭제", description="상품 리뷰를 삭제합니다.", responses=OpenApiResponse(WishlistSerializer)
+        summary="찜 목록 삭제", description="찜 목록을 삭제합니다.", responses=OpenApiResponse(WishlistSerializer)
     ),
 }
